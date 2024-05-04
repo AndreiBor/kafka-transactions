c08ce3c5062d868f9ccf684eab64287bf88cf71d
Andrei Borisov <andrei.borr@gmail.com>
2024-05-04 15:17:43 +0300

    Finish transactions

--- TransferService/src/main/java/by/javaguru/estore/transfers/service/TransferServiceImpl.java
+++ TransferService/src/main/java/by/javaguru/estore/transfers/service/TransferServiceImpl.java
@@ -10,64 +10,99 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import by.javaguru.estore.transfers.error.TransferServiceException;
 import by.javaguru.estore.transfers.model.TransferRestModel;
 
+import java.net.ConnectException;
+
 @Service
 public class TransferServiceImpl implements TransferService {
-	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-	private KafkaTemplate<String, Object> kafkaTemplate;
-	private Environment environment;
-	private RestTemplate restTemplate;
+    private KafkaTemplate<String, Object> kafkaTemplate;
+    private Environment environment;
+    private RestTemplate restTemplate;
 
-	public TransferServiceImpl(KafkaTemplate<String, Object> kafkaTemplate, Environment environment,
-			RestTemplate restTemplate) {
-		this.kafkaTemplate = kafkaTemplate;
-		this.environment = environment;
-		this.restTemplate = restTemplate;
-	}
+    public TransferServiceImpl(KafkaTemplate<String, Object> kafkaTemplate, Environment environment,
+                               RestTemplate restTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.environment = environment;
+        this.restTemplate = restTemplate;
+    }
 
-	@Override
-	public boolean transfer(TransferRestModel transferRestModel) {
-		WithdrawalRequestedEvent withdrawalEvent = new WithdrawalRequestedEvent(transferRestModel.getSenderId(),
-				transferRestModel.getRecepientId(), transferRestModel.getAmount());
-		DepositRequestedEvent depositEvent = new DepositRequestedEvent(transferRestModel.getSenderId(),
-				transferRestModel.getRecepientId(), transferRestModel.getAmount());
+    /*@Override
+    @Transactional
+    public boolean transfer(TransferRestModel transferRestModel) {
+        WithdrawalRequestedEvent withdrawalEvent = new WithdrawalRequestedEvent(transferRestModel.getSenderId(),
+                transferRestModel.getRecepientId(), transferRestModel.getAmount());
+        DepositRequestedEvent depositEvent = new DepositRequestedEvent(transferRestModel.getSenderId(),
+                transferRestModel.getRecepientId(), transferRestModel.getAmount());
 
-		try {
-			kafkaTemplate.send(environment.getProperty("withdraw-money-topic", "withdraw-money-topic"),
-					withdrawalEvent);
-			LOGGER.info("Sent event to withdrawal topic.");
+        try {
+            kafkaTemplate.send(environment.getProperty("withdraw-money-topic", "withdraw-money-topic"),
+                    withdrawalEvent);
+            LOGGER.info("Sent event to withdrawal topic.");
 
-			// Business logic that causes and error
-			callRemoteServce();
+            // Business logic that causes and error
+            callRemoteServce();
 
-			kafkaTemplate.send(environment.getProperty("deposit-money-topic", "deposit-money-topic"), depositEvent);
-			LOGGER.info("Sent event to deposit topic");
+            kafkaTemplate.send(environment.getProperty("deposit-money-topic", "deposit-money-topic"), depositEvent);
+            LOGGER.info("Sent event to deposit topic");
 
-		} catch (Exception ex) {
-			LOGGER.error(ex.getMessage(), ex);
-			throw new TransferServiceException(ex);
-		}
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage(), ex);
+            throw new TransferServiceException(ex);
+        }
 
-		return true;
-	}
+        return true;
+    }*/
 
-	private ResponseEntity<String> callRemoteServce() throws Exception {
-		String requestUrl = "http://localhost:8082/response/200";
-		ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, String.class);
+    @Override
+    @Transactional
+    public boolean transfer(TransferRestModel transferRestModel) {
+        WithdrawalRequestedEvent withdrawalEvent = new WithdrawalRequestedEvent(transferRestModel.getSenderId(),
+                transferRestModel.getRecepientId(), transferRestModel.getAmount());
+        DepositRequestedEvent depositEvent = new DepositRequestedEvent(transferRestModel.getSenderId(),
+                transferRestModel.getRecepientId(), transferRestModel.getAmount());
 
-		if (response.getStatusCode().value() == HttpStatus.SERVICE_UNAVAILABLE.value()) {
-			throw new Exception("Destination Microservice not availble");
-		}
+        try {
+            boolean result = kafkaTemplate.executeInTransaction(t -> {
 
-		if (response.getStatusCode().value() == HttpStatus.OK.value()) {
-			LOGGER.info("Received response from mock service: " + response.getBody());
-		}
-		return response;
-	}
+                t.send(environment.getProperty("withdraw-money-topic", "withdraw-money-topic"),
+                        withdrawalEvent);
+                LOGGER.info("Sent event to withdrawal topic.");
+
+                t.send(environment.getProperty("deposit-money-topic", "deposit-money-topic"), depositEvent);
+                LOGGER.info("Sent event to deposit topic");
+
+                return true;
+            });
+
+            callRemoteServce();
+
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage(), ex);
+            throw new TransferServiceException(ex);
+        }
+
+        return true;
+    }
+
+    private ResponseEntity<String> callRemoteServce() throws Exception {
+        String requestUrl = "http://localhost:8082/response/200";
+        ResponseEntity<String> response = restTemplate.exchange(requestUrl, HttpMethod.GET, null, String.class);
+
+        if (response.getStatusCode().value() == HttpStatus.SERVICE_UNAVAILABLE.value()) {
+            throw new Exception("Destination Microservice not availble");
+        }
+
+        if (response.getStatusCode().value() == HttpStatus.OK.value()) {
+            LOGGER.info("Received response from mock service: " + response.getBody());
+        }
+        return response;
+    }
 
 }
